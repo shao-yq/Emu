2a5b108e88c5528363ae07d1dbe910cb3a43bf20
Shao Yongqing <shyq@yahoo.com>
2018-05-25 14:40:58 +0800

    Module Config

--- CpuEmu/X86/src/cc/emulator/x86/i8086/instruction/MoveString.java
+++ CpuEmu/X86/src/cc/emulator/x86/i8086/instruction/MoveString.java
@@ -53,10 +53,12 @@ public class MoveString extends Instruction8086 {
     }
     @Override
     public String getMnemonic() {
-//        switch (op){
-//            case MOVS_STR8_STR8  : //   0xa4: // MOVS DEST-STR8,SRC-STR8
-//            case MOVS_STR16_STR16: //   0xa5: // MOVS DEST-STR16,SRC-STR16
-//        }
+        switch (op){
+            case MOVS_STR8_STR8  : //   0xa4: // MOVS DEST-STR8,SRC-STR8
+                return "MOVSB";
+            case MOVS_STR16_STR16: //   0xa5: // MOVS DEST-STR16,SRC-STR16
+                return "MOVSW";
+        }
         return "MOVS";
     }
 }
